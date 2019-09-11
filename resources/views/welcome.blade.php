45af20d6ccadb9ec4b443f0f6ddb3fcff473f228
ilhamsj <ilhamsaputrajati@gmail.com>
2019-09-12 02:16:48 +0700

    controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title') - {{env('app_name')}}</title>
-    <meta name="csrf_token" content="{{csrf_token()}}">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-</head>
-<body>
-    h2lp
-    <script src="{{asset('js/app.js')}}"></script>
-</body>
-</html>
+@extends('layouts.master')
+
+@section('title', 'Home')
+    
+@section('content')
+    <div class="container">
+        <h1>Helo</h1>
+    </div>
+@endsection
