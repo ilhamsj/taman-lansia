27cdd0a25777653605c0ada92b8d2eda9bc30033
ilhamsj <ilhamsaputrajati@gmail.com>
2019-09-25 22:46:41 +0700

    aos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,43 +1,3 @@
-@php
-    $about = [
-        'Tujuan' => [
-            '0' => 'Memberi wadah serta mengkondisikan perangkat untuk menata usia senja yang lebih berkualitas, baik dalam beribadah maupaun mengoptimalkan kemanfaatan hidupnya',
-        ],
-        'Motto & Motivasi' => [
-            '0' => 'mempersiapkan insan “utama dan cerdas di usia senja”',
-        ],
-        'Perkenalan' => [
-            '0' => 'Kami Adalah Tempat Pembelajaran, Pendidikan, Pelatihan, Pendampingan Bagi Santri-Santri Usia Senja, Untuk Memperdalam Agama, Mempersiapkan Diri Menghadapi Akhir Hayat Insya Alloh Menuju Husnul Khotimah',
-            '1' => 'Taman Lansia An-Naba’ Bukan Panti Jompo, Bukan Panti Wreda. Tl An-Naba’ Merupakan Wadah Komunitas Yang Mengakomodasi Santri Usia Senja. Santri Usia Muda Mempersiapkan Hidup; Santri Usia Senja Mempersiapkan Mati. Santri Usia Senja Bukan Hanya Yang Dhuafa, Karena Bisa Saja Ia Sukses Duniawi, Namun Merasa Miskin Ukhrowi.',
-            '3' => '
-            Lelaki Anshar Bertanya Kepada Rosulullah Saw : 
-                <blockquote class="blockquote">
-                Ya Rosulullah, Mukmin Manakah Yang Paling Utama ? Beliau Menjawab: Yang Paling Baik Akhlaknya Di Antara Mereka. Mukmin Manakah Yang Paling Cerdas ?, Tanya Lelaki Itu Lagi; Beliau Menjawab : Orang Yang Paling Banyak Mengingat Mati Dan Paling Baik Persiapanya Untuk Kehidupan Setelah Mati. Mereka Itulah Oran-Orang Yang Cerdas”
-                <footer class="blockquote-footer">Hr. Ibnu Majah No. 4259, Dihasankan 
-                        <cite title="Source Title">Asy-Syaih Al-Albani Ra Dalam Ash-Shahihah No. 1384</cite></footer></blockquote>',
-        ],
-        'Sasaran' => [
-            '0' => 'Muslim laki-laki maupun perempuan',
-            '1' => 'Usia 50 tahun ke atas',
-            '2' => 'bisa beraktifitas fisik maupun fikir',
-        ],
-        'Standar Pengasuhan' => [
-            '0' => 'Mengelola dan mengasuh santri-santri usia senja seperti berbakti kepada orang tua sendiri, yaitu standar BIRUL WALIDAIN (berbakti kepada kedua orang tua).',
-            '1' => 'Belajar Al-Quran dasar Tahsin',
-            '2' => 'Tafsir Hadist Fiqih Taddabur Ruqyah',
-            '3' => 'praktek Konseling',
-        ],
-        'Target' => [
-            '0' => 'Mencintai Al-Quran',
-            '1' => 'Rajin ibadah dan sholat lima waktu menjadi penantian',
-            '2' => 'Amalan-amalan sunnah menjadi kecintaan',
-            '3' => 'Husnul khotimah menjadi dambaan',
-            '4' => 'Ketrampilan tambahan sebagai kreatifitas sambilan',
-            '5' => 'Menata senja meniti surga menjadi tujuan',
-        ],
-    ];
-@endphp
-
 @extends('layouts.master')
 @section('title', 'Mempersiapkan insan utama dan cerdas di usia senja')
 @section('content')
@@ -46,8 +6,8 @@
 <div class="container">
     <div class="row h-100 align-items-center justify-content-center text-center">
         <div class="col-12 col-sm-8">
-            <h1 class="dispay-2">Taman Lansia An-naba</h1>
-            <p class="lead">
+            <h1 data-aos="slide-left" class="dispay-2">Taman Lansia An-naba</h1>
+            <p data-aos="slide-right" class="lead">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis, quisquam tenetur tempore aliquid itaque quas natus excepturi eaque labore molestias mollitia atque quos, ducimus impedit ad vitae sunt perferendis! Pariatur?
             </p>
         </div>
@@ -57,7 +17,7 @@
 
 <div id="about-us">
 @foreach ($about as $key => $value)    
-<div class="container mb-4">
+<div  data-aos="zoom-in" class="container mb-4">
     <div class="row h-100 align-items-center justify-content-center">
         <div class="col-md-10">
             <div id="{{ Str::slug($key) }}" class="card border-0 shadow" style="border-radius:1.25rem">
@@ -67,9 +27,9 @@
                     </div>
                     <div class="col-md">
                         <div class="card-body">
-                            <h1>{{$key}}</h1>
+                            <h1 data-aos="slide-down">{{$key}}</h1>
                             @foreach ($value as $v)
-                                <p class="card-text text-muted lead">
+                                <p data-aos="flip-up" class="card-text text-muted lead">
                                     {!! $v !!}
                                 </p>
                             @endforeach
--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,47 @@
 */
 
 Route::get('/', function () {
+    $about = [
+        'Tujuan' => [
+            '0' => 'Memberi wadah serta mengkondisikan perangkat untuk menata usia senja yang lebih berkualitas, baik dalam beribadah maupaun mengoptimalkan kemanfaatan hidupnya',
+        ],
+        'Motto & Motivasi' => [
+            '0' => 'mempersiapkan insan “utama dan cerdas di usia senja”',
+        ],
+        'Perkenalan' => [
+            '0' => 'Kami Adalah Tempat Pembelajaran, Pendidikan, Pelatihan, Pendampingan Bagi Santri-Santri Usia Senja, Untuk Memperdalam Agama, Mempersiapkan Diri Menghadapi Akhir Hayat Insya Alloh Menuju Husnul Khotimah',
+            '1' => 'Taman Lansia An-Naba’ Bukan Panti Jompo, Bukan Panti Wreda. Tl An-Naba’ Merupakan Wadah Komunitas Yang Mengakomodasi Santri Usia Senja. Santri Usia Muda Mempersiapkan Hidup; Santri Usia Senja Mempersiapkan Mati. Santri Usia Senja Bukan Hanya Yang Dhuafa, Karena Bisa Saja Ia Sukses Duniawi, Namun Merasa Miskin Ukhrowi.',
+            '3' => '
+            Lelaki Anshar Bertanya Kepada Rosulullah Saw : 
+                <blockquote class="blockquote" data-aos="flip-up">
+                Ya Rosulullah, Mukmin Manakah Yang Paling Utama ? Beliau Menjawab: Yang Paling Baik Akhlaknya Di Antara Mereka. Mukmin Manakah Yang Paling Cerdas ?, Tanya Lelaki Itu Lagi; Beliau Menjawab : Orang Yang Paling Banyak Mengingat Mati Dan Paling Baik Persiapanya Untuk Kehidupan Setelah Mati. Mereka Itulah Oran-Orang Yang Cerdas”
+                <footer class="blockquote-footer">Hr. Ibnu Majah No. 4259, Dihasankan 
+                        <cite title="Source Title">Asy-Syaih Al-Albani Ra Dalam Ash-Shahihah No. 1384</cite></footer></blockquote>',
+        ],
+        'Sasaran' => [
+            '0' => 'Muslim laki-laki maupun perempuan',
+            '1' => 'Usia 50 tahun ke atas',
+            '2' => 'bisa beraktifitas fisik maupun fikir',
+        ],
+        'Standar Pengasuhan' => [
+            '0' => 'Mengelola dan mengasuh santri-santri usia senja seperti berbakti kepada orang tua sendiri, yaitu standar BIRUL WALIDAIN (berbakti kepada kedua orang tua).',
+            '1' => 'Belajar Al-Quran dasar Tahsin',
+            '2' => 'Tafsir Hadist Fiqih Taddabur Ruqyah',
+            '3' => 'praktek Konseling',
+        ],
+        'Target' => [
+            '0' => 'Mencintai Al-Quran',
+            '1' => 'Rajin ibadah dan sholat lima waktu menjadi penantian',
+            '2' => 'Amalan-amalan sunnah menjadi kecintaan',
+            '3' => 'Husnul khotimah menjadi dambaan',
+            '4' => 'Ketrampilan tambahan sebagai kreatifitas sambilan',
+            '5' => 'Menata senja meniti surga menjadi tujuan',
+        ],
+    ];
+
     return view('welcome')->with([
-        'items' => \App\Article::paginate(6)
+        'items' => \App\Article::paginate(6),
+        'about' => $about
     ]);
 })->name('/');
 
